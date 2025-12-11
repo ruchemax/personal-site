4db9aabb0238438341d8b5304368649858cf78f3
ruchemax <ruchemax@list.ru>
2025-12-11 01:08:05 +0100

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import tailwind from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://www.maxruchkin.org',
   output: 'server',
   adapter: vercel(),
   vite: {
     plugins: [tailwind()],
   },
+  integrations: [
+    sitemap()
+  ],
 });
