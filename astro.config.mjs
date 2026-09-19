403f692a4182fc4e74085f743aa6c85983082830
ruchemax <ruchemax@list.ru>
2026-09-19 17:34:02 +0200

    Rename local history document routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ export default defineConfig({
   site: 'https://www.maxruchkin.org',
   output: 'server',
   adapter: vercel(),
+  redirects: {
+    '/ru/local_history/chebarkul-register-1760': '/ru/local_history/chebarkul-metrika-1760',
+    '/ru/local_history/chebarkul-register-1771': '/ru/local_history/chebarkul-metrika-1771',
+    '/ru/local_history/chebarkul-confession-1773': '/ru/local_history/chebarkul-ispoved-1773',
+  },
   vite: {
     plugins: [tailwind()],
   },
